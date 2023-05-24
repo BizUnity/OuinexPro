a1ece999bf01539a3ea526f36f34780ceed6e92e
BizUnity <128179084+BizUnity@users.noreply.github.com>
2023-05-24 15:13:56 +0200

    add charts

---
+++ OuinexPro.SharedViews/Views/ChartView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OuinexPro.SharedViews.Views;
+
+public partial class ChartView : UserControl
+{
+    public ChartView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ OuinexPro.SharedViews/Views/SimpleMarketWatch.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OuinexPro.SharedViews.Views;
+
+public partial class SimpleMarketWatch : UserControl
+{
+    public SimpleMarketWatch()
+    {
+        InitializeComponent();
+    }
+}
